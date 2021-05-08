b8b64fa399fbd44018b0acc019fbbfeb335e4f72
KangDroid <hyunwoo286@gmail.com>
2021-05-09 00:41:25 +0900

    ServerConfiguration: Initiate Client-Side-Configuration

--- src/main/kotlin/com/libraryclient/libraryclient/LibraryClientApplication.kt
+++ src/main/kotlin/com/libraryclient/libraryclient/LibraryClientApplication.kt
@@ -1,8 +1,11 @@
 package com.libraryclient.libraryclient
 
+import com.libraryclient.libraryclient.configuration.ServerConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableConfigurationProperties(ServerConfiguration::class)
 @SpringBootApplication
 class LibraryClientApplication
 
---
+++ src/main/kotlin/com/libraryclient/libraryclient/configuration/ServerConfiguration.kt
@@ -0,0 +1,25 @@
+package com.libraryclient.libraryclient.configuration
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import javax.annotation.PostConstruct
+
+@ConstructorBinding
+@ConfigurationProperties("cs-library")
+
+class ServerConfiguration(
+    private val serverScheme: String,
+    private val serverAddress: String,
+    private val serverPort: String
+) {
+    lateinit var serverBaseAddress: String
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @PostConstruct
+    fun initServerAddress() {
+        serverBaseAddress = "$serverScheme://$serverAddress:$serverPort"
+        logger.info("Initiated server address: $serverBaseAddress")
+    }
+}
