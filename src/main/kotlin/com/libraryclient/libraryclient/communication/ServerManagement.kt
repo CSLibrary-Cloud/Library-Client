e26ef4eebe894d8afe51894881e9c1c6eab0010a
KangDroid <hyunwoo286@gmail.com>
2021-05-09 00:44:41 +0900

    ServerManagement: Use Server Configuration instead

--- src/main/kotlin/com/libraryclient/libraryclient/communication/ServerManagement.kt
+++ src/main/kotlin/com/libraryclient/libraryclient/communication/ServerManagement.kt
@@ -2,6 +2,7 @@ package com.libraryclient.libraryclient.communication
 
 import com.cslibrary.library.data.dto.response.LoginResponse
 import com.cslibrary.library.data.dto.response.RegisterResponse
+import com.libraryclient.libraryclient.configuration.ServerConfiguration
 import com.libraryclient.libraryclient.data.request.LoginRequest
 import com.libraryclient.libraryclient.data.request.RegisterRequest
 import org.springframework.http.ResponseEntity
@@ -10,19 +11,19 @@ import org.springframework.web.client.postForEntity
 
 
 class ServerManagement (
-    var serverAddress: String = "",     //TODO server Address needed
-    var restTemplate: RestTemplate
+    private val serverConfiguration: ServerConfiguration,
+    private val restTemplate: RestTemplate
 ) {
 
     fun signUpCommunication(registerRequest: RegisterRequest) : RegisterResponse {
         val registerResponse: ResponseEntity<RegisterResponse> =
-            restTemplate.postForEntity(serverAddress, registerRequest, RegisterResponse::class)
+            restTemplate.postForEntity(serverConfiguration.serverBaseAddress, registerRequest, RegisterResponse::class)
         return registerResponse.body!!
     }
 
     fun loginCommunication(loginRequest: LoginRequest) : LoginResponse {
         val loginResponse: ResponseEntity<LoginResponse> =
-            restTemplate.postForEntity(serverAddress, loginRequest, LoginResponse::class)
+            restTemplate.postForEntity(serverConfiguration.serverBaseAddress, loginRequest, LoginResponse::class)
         return loginResponse.body!!
     }
 }
