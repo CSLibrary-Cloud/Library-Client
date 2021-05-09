af01b6f30cbcf6d966ec042b47635deebac93fdf
KangDroid <hyunwoo286@gmail.com>
2021-05-09 16:26:07 +0900

    Gradle: Disable mongoDB on client

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation(kotlin("test-junit"))
