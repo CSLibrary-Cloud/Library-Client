fd0df9025820bdbf1c3211deeb2b6ec510859011
mimwin <kms105903@naver.com>
2021-05-15 15:32:07 +0900

    SecondPage.kt : Menu (After the Login)

---
+++ src/main/kotlin/com/cslibrary/client/ui/SecondPage.kt
@@ -0,0 +1,38 @@
+package com.cslibrary.client.ui
+
+import java.util.*
+
+class SecondPage (
+    private val shape : Shape,
+    private val seat : Seat,
+){
+
+    fun secondPage(){
+        val scanner : Scanner = Scanner(System.`in`)
+        while(true){
+            shape.makeRec(3, "Second Page")
+            println("1. Show Seat\n2. Select Seat\n3. Change User State\n4.Report User\n5.Extend Time\n6.Main Page")
+            print("Choose the number : ")
+            val ch = scanner.next()
+            when(ch!!) {
+                "1" -> {
+                    println("Show Seat Selected")
+                    seat.showSeat()
+                }
+                "2"->{
+                    println("Select Seat Selected")
+                    seat.chooseSeat()
+                }
+                //3,4
+                "6"->{
+                    println("Return to Main Page")
+                    return
+                }
+                null ->{
+                    println("다시 입력해주세요.")
+                }
+            }
+        }
+    }
+
+}
