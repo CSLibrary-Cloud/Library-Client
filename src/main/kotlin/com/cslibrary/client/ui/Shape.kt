dd4cf69f6d2770d4fb51c9055df17f29b3cdd3f0
mimwin <kms105903@naver.com>
2021-05-15 15:34:50 +0900

    Shape.kt : Seat UI

--- src/main/kotlin/com/cslibrary/client/ui/Shape.kt
+++ src/main/kotlin/com/cslibrary/client/ui/Shape.kt
@@ -1,5 +1,6 @@
 package com.cslibrary.client.ui
 
+import com.cslibrary.client.data.response.SeatResponse
 import org.springframework.stereotype.Component
 
 @Component
@@ -14,6 +15,26 @@ class Shape {
         print("\n$str\n")
         for (i in 0..num*2){
             print("-- ")
+        }
+        print("\n")
+    }
+
+    fun makeSeat(arr: List<SeatResponse>){
+        var isUs : String = ""
+
+        makeRec(2,"SEAT")
+
+        for (i in arr.indices){
+            //NxN 에서 N 만큼 나누기
+            if(arr[i].isUsing) isUs = "T"
+            else isUs = "F"
+
+            if(arr[i].seatNumber<9)
+                print("${arr[i].seatNumber+1}  > $isUs | ")
+            else
+                print("${arr[i].seatNumber+1} > $isUs | ")
+
+            if(i>0 && (i+1)%5==0)print("\n")
         }
         print("\n")
     }
