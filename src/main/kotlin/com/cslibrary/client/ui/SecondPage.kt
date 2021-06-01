9caeb55a51d027e88a42b420cc2dfb2a9ba6bf27
mimwin <kms105903@naver.com>
2021-06-01 18:03:52 +0900

    secondPage, ThirdPage UI

--- src/main/kotlin/com/cslibrary/client/ui/SecondPage.kt
+++ src/main/kotlin/com/cslibrary/client/ui/SecondPage.kt
@@ -6,33 +6,13 @@ class SecondPage (
     private val shape : Shape,
     private val seat : Seat,
 ){
-
     fun secondPage(){
         val scanner : Scanner = Scanner(System.`in`)
-        while(true){
-            shape.makeRec(3, "Second Page")
-            println("1. Show Seat\n2. Select Seat\n3. Change User State\n4.Report User\n5.Extend Time\n6.Main Page")
-            print("Choose the number : ")
-            val ch = scanner.next()
-            when(ch!!) {
-                "1" -> {
-                    println("Show Seat Selected")
-                    seat.showSeat()
-                }
-                "2"->{
-                    println("Select Seat Selected")
-                    seat.chooseSeat()
-                }
-                //3,4
-                "6"->{
-                    println("Return to Main Page")
-                    return
-                }
-                null ->{
-                    println("다시 입력해주세요.")
-                }
-            }
-        }
+        val realtime = RealTimePage(shape,seat)
+        shape.makeRec(3, "Select Seat")
+        seat.chooseSeat(2)
+        val time = 3
+        realtime.realtime()
+        return
     }
-
 }
---
+++ src/main/kotlin/com/cslibrary/client/ui/ThirdPage.kt
@@ -0,0 +1,39 @@
+package com.cslibrary.client.ui
+import java.util.*
+
+class ThirdPage(
+    private val shape : Shape,
+    private val seat : Seat,
+) {
+
+    fun thirdPage() : Int{
+        val sc : Scanner = Scanner(System.`in`)
+        while(true){
+            shape.makeRec(3, "Third Page")
+            println("1. Show Seat\n2. Change Seat\n3.Report User\n4.Extend Time\n5.Timer & LeaderBoard\n6.Main Page")
+            print("Choose the number : ")
+            when(sc.next()) {
+                "1" -> {
+                    println("Show Seat Selected")
+                    seat.showSeat()
+                }
+                "2"->{
+                    println("Change Seat Selected")
+                    seat.chooseSeat(3)
+                }
+                //3= 신고, 4= 시간연장
+                "5"->{
+                    println("Timer & LeaderBoard Selected")
+                    return 5
+                }
+                "6"->{
+                    println("Return to Main Page")
+                    return 6
+                }
+                null ->{
+                    println("다시 입력해주세요.")
+                }
+            }
+        }
+    }
+}
